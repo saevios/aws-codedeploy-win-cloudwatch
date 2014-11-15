862e574b86bec54e0b31d3b77401330b4ba9627c
Ryuta Otaki <otaki.ryuta@classmethod.jp>
2014-11-15 15:54:59 +0900

    first commit

---
+++ remove_previous_configs.bat
@@ -0,0 +1,2 @@
+del "C:\Program Files\Amazon\Ec2ConfigService\Settings\AWS.EC2.Windows.CloudWatch.json"
+del "C:\Program Files\Amazon\Ec2ConfigService\Settings\config.xml"
---
+++ restart_service.bat
@@ -0,0 +1,2 @@
+net stop ec2config
+net start ec2config
